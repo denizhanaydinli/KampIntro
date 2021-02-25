b79effef8b17d6179abfa53220b702f6a69a38ad
Denizhan Aydinli <denizhanaydinli@gmail.com>
2021-02-25 17:20:56 +0300

    commit1

---
+++ OOP/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.Id = 1;
+            product.ProductName = "Masa";
+            product.CategoryId = 2;
+            product.UnitPrice = 500;
+            product.UnitsInStock = 3;
+
+            Product product1 = new Product
+            {
+                Id = 2,
+                CategoryId = 5,
+                UnitsInStock = 5,
+                ProductName = "Kalem",
+                UnitPrice = 35
+            };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product);
+        }
+    }
+}
